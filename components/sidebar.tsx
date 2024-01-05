3f91c01181e7bd39da0e70897d361d38ef563291
Nitin Dabi <Nitindabi954@gmail.com>
2024-01-05 21:24:11 +0530

    update sidebar.tsx

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -4,10 +4,58 @@ import Link from "next/link"
 import Image from "next/image";
 import { Montserrat } from "next/font/google"
 
+import { cn } from "@/lib/utils";
+import { Code, ImageIcon, LayoutDashboard, MessageSquare, MusicIcon, Settings, VideoIcon } from "lucide-react";
+
 const montserrat = Montserrat({
-    weight: "600", 
+    weight: "600",
     subsets: ["latin"]
 })
+
+
+const routes = [
+    {
+        label: "Dashboard",
+        icon: LayoutDashboard,
+        href: "/dashboard",
+        color: "text-sky-500"
+    },
+    {
+        label: "Conversation",
+        icon: MessageSquare,
+        href: "/conversation",
+        color: "text-violet-500"
+    },
+    {
+        label: "Image Generation",
+        icon: ImageIcon,
+        href: "/image",
+        color: "text-Pink-700"
+    },
+    {
+        label: "Video Generation",
+        icon: VideoIcon,
+        href: "/video",
+        color: "text-orange-700"
+    },
+    {
+        label: "Music Generation",
+        icon: MusicIcon,
+        href: "/music",
+        color: "text-emerald-500"
+    },
+    {
+        label: "Code Generation",
+        icon: Code,
+        href: "/code",
+        color: "text-green-700"
+    },
+    {
+        label: "Settings",
+        icon: Settings,
+        href: "/setting",
+    },
+];
 
 const Sidebar = () => {
     return (
@@ -17,9 +65,26 @@ const Sidebar = () => {
                     <div className="relative h-8 w-8 mr-4">
                         <Image fill alt="Logo" src="/logo.png" />
                     </div>
-                    <h1 className="text=2xl font-bold">Nitin Dabi</h1>
+                    <h1 className={cn("text=2xl font-bold", montserrat.className)}>
+                        Nitin Dabi
+                    </h1>
                 </Link>
-
+                <div className="space-y-1">
+                    {routes.map((route) => (
+                        <Link
+                            href={route.href}
+                            key={route.href}
+                            className="text-sm group flex p-3 w-full
+                            justify-start font-medium cursor-pointer
+                            hover:text-white hover:bg-white/10 rounded-lg"
+                        >
+                            <div className="flex items-center flex-1">
+                                <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
+                                {route.label}
+                            </div>
+                        </Link>
+                    ))}
+                </div>
             </div>
 
         </div>
