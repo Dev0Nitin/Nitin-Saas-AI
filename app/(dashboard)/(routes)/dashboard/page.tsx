bfdde8d6ea8617d23a4beeb98b4885768b729c18
Nitin Dabi <Nitindabi954@gmail.com>
2024-01-10 07:24:43 +0530

    Adding Card on  Dashboard

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,14 +1,96 @@
-import { UserButton } from "@clerk/nextjs"
+"use client";
+
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { ArrowRight, Code, ImageIcon, MessageSquare, Music, MusicIcon, VideoIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+const tools = [
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+    href: "/coversation"
+  },
+  {
+    label: "Image Generation",
+    icon: ImageIcon,
+    color: "text-pink-700",
+    bgColor: "bg-pink-500/10",
+    href: "/Image-Generation"
+  },
+  {
+    label: "Video Generation",
+    icon: VideoIcon,
+    color: "text-orange-500",
+    bgColor: "bg-orange-500/10",
+    href: "/Video-Generation"
+  },
+  {
+    label: "Music Generation",
+    icon: Music,
+    color: "text-emerald-500",
+    bgColor: "bg-emerald-500/10",
+    href: "/Music-Generation"
+  },
+  {
+    label: "Code Generation",
+    icon: Code,
+    color: "text-green-500",
+    bgColor: "bg-green-500/10",
+    href: "/Code-Generation"
+  }
+]
 
 const DashboardPage = () => {
+
+  const router = useRouter(); // This is used for when we click it will redirect to next page
   return (
     <div>
-        <p>Dashboard Page (Protected)</p>
-        
-    </div>
-    
 
-    )
+      <div className="md-8 space-y-4">
+        <h2 className="text-2xl md:4xl front-bold text-center">
+          Explore the power of AI
+        </h2>
+        <p className="text-muted-foreground font-light text-sm
+        md:ext-lg text-center">
+          Chat with the smartest AI - Experience the power of AI
+        </p>
+      </div>
+      <div className="px-4 md:px=20 lg:px-32 space-y-4">
+
+        {/* Accessing the tools one by one */}
+        {tools.map((tool) => (
+
+          // Creating a Card
+          <Card
+            onClick={() => router.push(tool.href)}
+            key={tool.href}
+            className="p-4 border-black/5 flex item-center justify-between 
+          hover:shadow-md transition cursor-pointer"
+          >
+            <div className="flex item-center gap-x-4">
+              <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                <tool.icon className={cn("w-8 h-8", tool.color)} />
+              </div>
+
+              <div className="font-semibold">
+                {tool.label}
+              </div>
+            </div>
+
+            {/* Creating a Arrow in each tool navigation */}
+            <ArrowRight className="w-5 h-5" />
+          </Card>
+        ))}
+      </div>
+
+    </div>
+
+
+
+  )
 }
 
 export default DashboardPage
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -6,6 +6,7 @@ import { Montserrat } from "next/font/google"
 
 import { cn } from "@/lib/utils";
 import { Code, ImageIcon, LayoutDashboard, MessageSquare, MusicIcon, Settings, VideoIcon } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 const montserrat = Montserrat({
     weight: "600",
@@ -58,6 +59,9 @@ const routes = [
 ];
 
 const Sidebar = () => {
+
+    const pathname = usePathname(); // This will highlight the text in Navigation bar for current page.
+
     return (
         <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
             <div className="px-3 py-2 flex-1">
@@ -74,9 +78,9 @@ const Sidebar = () => {
                         <Link
                             href={route.href}
                             key={route.href}
-                            className="text-sm group flex p-3 w-full
-                            justify-start font-medium cursor-pointer
-                            hover:text-white hover:bg-white/10 rounded-lg"
+                            className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg", 
+                            pathname === route.href ? "text=white bg-white/10" : "text-zinc-400"
+                            )}
                         >
                             <div className="flex items-center flex-1">
                                 <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
